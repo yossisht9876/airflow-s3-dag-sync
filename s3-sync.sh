fe044515d6f0b84ebff621ef27f8947a2d2676e8
yossisht9876 <yossi.shtein@gmail.com>
2022-04-24 13:10:12 +0300

    Update s3-sync.sh

--- s3-sync.sh
+++ s3-sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-while true; do aws s3 sync s3://airflow-stg-s3-log-bucket/airflow_home/dags/ opt/airflow/dags/repo/; sleep 60; done
+while true; do aws s3 sync s3://<bucket-name>/ <airflow-dags-home-folder>/; sleep 60; done
