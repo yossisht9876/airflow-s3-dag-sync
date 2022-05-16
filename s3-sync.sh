68a5c24e40970b722096efd62145bb7afd9d6246
yossisht9876 <yossi.shtein@gmail.com>
2022-05-16 16:54:09 +0300

    Update s3-sync.sh
    works with subfolders

--- s3-sync.sh
+++ s3-sync.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
-
-while true; do aws s3 sync s3://<bucket-name>/ <airflow-dags-home-folder>/; sleep 60; done
+#!/bin/bash
+while true; do
+aws s3 ls lusha-airflow-stg-dags --recursive | awk '{if ($3 !=0) print "<Bucket-Name>/"$4}' > s3.txt
+while IFS= read -r file; do
+    aws s3 cp "$file" "opt/airflow/dags/repo/"
+done < s3.txt;
+echo sleeping && sleep 60  && rm ./s3.txt;
+done;
