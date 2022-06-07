cd2d64c65c95f55a52aca6412cb83d7401523d18
yossisht9876 <yossi.shtein@gmail.com>
2022-06-07 12:04:38 +0300

    Update s3-sync.sh

--- s3-sync.sh
+++ s3-sync.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 while true; do
-aws s3 ls lusha-airflow-stg-dags --recursive | awk '{if ($3 !=0) print "<Bucket-Name>/"$4}' > s3.txt
+
+# checking if new file added to the bucket - if yes - it will upload it the dag bag
+
+account_id=$(aws sts get-caller-identity --query "Account" --output text)
+if [ $account_id == "458600517589" ];
+then
+  BUCKET_NAME="airflow-prod-dags"
+  aws s3 ls "lusha-airflow-prod-dags" --recursive | awk '{if ($3 !=0) print "s3://airflow-prod-dags/"$4}' > s3.txt
+elif [ $account_id == "716811407690" ];
+then
+  BUCKET_NAME="airflow-stg-dags"
+  aws s3 ls "lusha-airflow-stg-dags" --recursive | awk '{if ($3 !=0) print "s3://airflow-stg-dags/"$4}' > s3.txt
+elif [ $account_id == "104001504630" ];
+then
+  BUCKET_NAME="airflow-dev-dags"
+  aws s3 ls "lusha-airflow-dev-dags" --recursive | awk '{if ($3 !=0) print "s3://airflow-dev-dags/"$4}' > s3.txt
+fi
+
 while IFS= read -r file; do
-    aws s3 cp "$file" "opt/airflow/dags/repo/"
+    aws s3 sync "$file" "opt/airflow/dags/repo/"
 done < s3.txt;
 echo sleeping && sleep 60  && rm ./s3.txt;
+
+# checking if dag file was removed from the s3 bucket - compare the dag bag and the s3
+# and if there is a diff it will remove it from the dag bag
+
+aws s3 ls "$BUCKET_NAME" --recursive | awk '{if ($3 !=0) print $4}' |  sed 's/.*[/:]//' > s3_diff.txt
+ls opt/airflow/dags/repo/ > dag_diff.txt
+sort s3_diff.txt dag_diff.txt | uniq -u > diff.txt
+
+while IFS= read -r file; do rm -rf "$file" "opt/airflow/dags/repo/"
+done < diff.txt;
+
+echo checking  && rm s3_diff.txt dag_diff.txt diff.txt && sleep 80
+
 done;
